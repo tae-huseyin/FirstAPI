a315ea4fcbd5907ff934b823dbe08b4cda7ff970
Vaguehorizon <Vaguehorizon@users.noreply.github.com>
2017-11-22 10:52:12 +0000

    uses permission and setServer in main activity

--- app/src/main/java/com/muelpatmore/firstapi/MainActivity.java
+++ app/src/main/java/com/muelpatmore/firstapi/MainActivity.java
@@ -3,11 +3,49 @@ package com.muelpatmore.firstapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.muelpatmore.firstapi.services.RequestInterface;
+import com.muelpatmore.firstapi.util.constants.CakeModel;
+
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestInterface requestInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestInterface.getCakesList()
+                .observeOn(AndroidSchedulers.mainThread())
+                        // ensure to reference plural Schedulers class
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<List<CakeModel>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(List<CakeModel> value) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
