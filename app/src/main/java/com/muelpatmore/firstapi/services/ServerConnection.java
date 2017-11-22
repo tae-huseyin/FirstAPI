4ef8e7e161d0aa4b62d22f692dab1e8c58dddc28
Vaguehorizon <Vaguehorizon@users.noreply.github.com>
2017-11-22 10:41:35 +0000

    Interface first request written. ServerConnection class created

---
+++ app/src/main/java/com/muelpatmore/firstapi/services/ServerConnection.java
@@ -0,0 +1,13 @@
+package com.muelpatmore.firstapi.services;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+/**
+ * Created by Samuel on 22/11/2017.
+ */
+
+public class ServerConnection {
+    private static Retrofit retrofit;
+    private static OkHttpClient okHttpClient;
+}
